1b6095eacb12ede00bbb047222840b5d5368e5b0
mac <kamenev.nikolaj2010@yandex.ru>
2022-04-18 20:55:19 +0300

    feat(): initial commite and first console program

---
+++ firstProjectCScharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace firstProjectCScharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Your name is {name}");
+        }
+    }
+}
