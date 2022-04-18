df0e94722782aef5b001559d280cfea8472e50a0
mac <kamenev.nikolaj2010@yandex.ru>
2022-04-18 21:16:20 +0300

    feat(): lesson about Variables

--- firstProjectCScharp/Program.cs
+++ firstProjectCScharp/Program.cs
@@ -6,9 +6,14 @@ namespace firstProjectCScharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("What's your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Your name is {name}");
+            string name = "Nikolay";
+            int age = 23;
+            bool isEmployed = true;
+            double weight = 75.34;
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"isEmployed: {isEmployed}");
+            Console.WriteLine($"Weight: {weight}");
         }
     }
 }
