b47c087d494fd61cbe74e02a9b2cb6d15c442cf9
mac <kamenev.nikolaj2010@yandex.ru>
2022-04-18 21:18:52 +0300

    feat(): learn numbers

--- firstProjectCScharp/Program.cs
+++ firstProjectCScharp/Program.cs
@@ -6,14 +6,13 @@ namespace firstProjectCScharp
     {
         public static void Main(string[] args)
         {
-            string name = "Nikolay";
-            int age = 23;
-            bool isEmployed = true;
-            double weight = 75.34;
-            Console.WriteLine($"Name: {name}");
-            Console.WriteLine($"Age: {age}");
-            Console.WriteLine($"isEmployed: {isEmployed}");
-            Console.WriteLine($"Weight: {weight}");
+            float a = 3.14f;
+            float b = 3.4345f;
+
+            decimal d = 3.434m;
+            uint c = 34u;
+            long e = 324561;
+            ulong f = 243526ul;
         }
     }
 }
